307965173472959cbdb19b96ecc353ce1cd0a101
ttingin <ttingin5745@gmail.com>
2024-02-21 23:55:47 +0900

    페이지 구현 일기 수정

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-
+import React, {useReducer, useRef} from "react";
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
-import New from "./pages/New";
 import Edit from "./pages/Edit";
+import New from "./pages/New";
 import Diary from "./pages/Diary";
-import React, {useReducer, useRef} from "react";
 
-const reducer = (state,action)=>{
+const reducer = (state, action) => {
     let newState = [];
-    switch (action.type){
-        case 'INIT' : {
+    switch (action.type) {
+        case 'INIT': {
             return action.data;
         }
-        case 'CREATE' :{
-            const newItem = {
-                ...action.data
-            };
-            newState = [newItem, ...state];
+        case 'CREATE': {
+            newState = [action.data, ...state];
             break;
         }
         case 'REMOVE': {
-            newState = state.filter((it)=>it.id !== action.targetId);
+            newState = state.filter((it) => it.id !== action.targetId);
             break;
         }
-        case 'EDIT' : {
-            newState = state.map((it)=>it.id === action.data.id? {...action.data}:it);
+        case 'EDIT': {
+            newState = state.map((it) => it.id === action.data.id ? {...action.data} : it);
             break;
         }
         default:
@@ -39,101 +35,98 @@ export const DiaryDispatchContext = React.createContext();
 
 const dummyData = [
     {
-        id:1,
-        emotion:1,
+        id: 1,
+        emotion: 1,
         content: "오늘의일기 1번",
-        date: 163546454849
+        date: 1707389662870,
     },
     {
-        id:2,
-        emotion:2,
+        id: 2,
+        emotion: 2,
         content: "오늘의일기 2번",
-        date: 163546454850
+        date: 1707389662871,
     },
     {
-        id:3,
-        emotion:3,
+        id: 3,
+        emotion: 3,
         content: "오늘의일기 3번",
-        date: 163546454853
+        date: 1707389662873,
     },
     {
-        id:4,
-        emotion:4,
+        id: 4,
+        emotion: 4,
         content: "오늘의일기 4번",
-        date: 163546454854
+        date: 1707389662874,
     },
     {
-        id:5,
-        emotion:5,
+        id: 5,
+        emotion: 5,
         content: "오늘의일기 5번",
-        date: 163546454855
+        date: 1707389662875,
     },
-    {
-        id:6,
-        emotion:6,
-        content: "오늘의일기 6번",
-        date: 163546454856
-    }
 ];
 
-function App() {
 
-    const [data,dispatch] = useReducer(reducer,dummyData);
+function App() {
+    console.log(new Date().getTime());
+
+    const [data, dispatch] = useReducer(reducer, dummyData);
     const dataId = useRef(0);
-    const  onCreate = (date, content, emotion) => {
+
+    //CREATE
+    const onCreate = (date, content, emotion) => {
         dispatch({
             type: "CREATE",
             data: {
-                id:dataId.current,
+                id: dataId.current,
                 date: new Date(date).getTime(),
                 content,
                 emotion,
             },
         });
         dataId.current += 1;
-    }
+    };
 
-    const  onRemove = (targetId) => {
+    //REMOVE
+    const onRemove = (targetId) => {
         dispatch({
             type: "REMOVE",
-            targetId
+            targetId,
         })
     };
 
-    const onEdit = (targetId,date,content,emotion) => {
+    const onEdit = (targetId, date, content, emotion) => {
         dispatch({
             type: "EDIT",
             data: {
                 id: targetId,
                 date: new Date(date).getTime(),
                 content,
-                emotion
+                emotion,
             },
         });
-    }
+    };
 
-  return (
-      <DiaryStateContext.Provider value={data}>
-          <DiaryDispatchContext.Provider value={
-              {
-                  onCreate,
-                  onEdit,
-                  onRemove
-              }
-          }>
-              <BrowserRouter>
-                <div className="App">
-                  <Routes>
-                        <Route path="/" element={<Home/>}></Route>
-                        <Route path="/new" element={<New/>}></Route>
-                        <Route path="/edit" element={<Edit/>}></Route>
-                        <Route path="/diary/:id" element={<Diary/>}></Route>
-                    </Routes>
-                </div>
-              </BrowserRouter>
-          </DiaryDispatchContext.Provider>
-      </DiaryStateContext.Provider>
-  );
+    return (
+        <DiaryStateContext.Provider value={data}>
+            <DiaryDispatchContext.Provider value={{
+                onCreate,
+                onEdit,
+                onRemove,
+            }}>
+                <BrowserRouter>
+                    <div className="App">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/new" element={<New />} />
+                            <Route path="/edit/:id" element={<Edit />} />
+                            <Route path="/diary/:id" element={<Diary />} />
+                        </Routes>
+                    </div>
+                </BrowserRouter>
+            </DiaryDispatchContext.Provider>
+        </DiaryStateContext.Provider>
+    );
 }
 
 export default App;
